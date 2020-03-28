a20af129e77fe1607eb295ef112fb0ec7ba8591a
aartikansal <aartilumb@gmail.com>
2020-03-28 18:37:19 -0400

    interface teacher created

---
+++ src/main/java/com/example/demo/Teacher.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.util.Iterator;
+
+public interface Teacher {
+    public void teach(Learner learner, double numberOfHours);
+    
+    public void lecture (Iterator<? extends Learner>learners, double numberOfHours);
+}
