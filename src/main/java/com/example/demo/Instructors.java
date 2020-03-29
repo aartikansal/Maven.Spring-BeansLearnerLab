6e654980753e28653161ed671a8cd02f7a129a9e
aartikansal <aartilumb@gmail.com>
2020-03-28 20:05:52 -0400

    instructors class created

---
+++ src/main/java/com/example/demo/Instructors.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class Instructors extends People<Instructor> {
+
+    public Instructors(List<Instructor> personList) {
+        super(personList);
+    }
+}
